ba7877185427d4f8d5f999f650fac342b8ff6f82
jlsp-zy <jlsp-zy@163.com>
2020-11-27 09:01:25 +0800

    222

--- src/main/java/com/atguigu/springboot/Love.java
+++ src/main/java/com/atguigu/springboot/Love.java
@@ -2,6 +2,6 @@ package com.atguigu.springboot;
 
 public class Love {
     public static void main(String[] args) {
-        System.out.println("同学们，辛苦啦");
+        System.out.println("同学们，辛苦啦222");
     }
 }
