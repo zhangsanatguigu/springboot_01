11b49943908f97b9d278a77fea872aa37d82b3fd
jlsp-zy <jlsp-zy@163.com>
2020-11-27 09:25:40 +0800

    修改了代码

--- src/main/java/com/atguigu/springboot/Love.java
+++ src/main/java/com/atguigu/springboot/Love.java
@@ -4,5 +4,6 @@ public class Love {
     public static void main(String[] args) {
         System.out.println("同学们，辛苦啦，111");
         System.out.println("同学们，辛苦啦222");
+        System.out.println("不辛苦");
     }
 }
